2a8a95d1f7d57b3f2b811bff519dbccd291a6d7d
b1n4ryw0rm <b1n4ryw0rm@gmail.com>
2023-10-08 02:10:17 +0530

    day1 solved

---
+++ hackerrank/30_days_of_code/day1/day1.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+input_int = int(input())
+input_double = float(input())
+input_string = input()
+
+sum_int = i + input_int
+sum_double = d + input_double
+concatenated_string = s + input_string
+
+print(sum_int)
+print("{:.1f}".format(sum_double))
+print(concatenated_string)
